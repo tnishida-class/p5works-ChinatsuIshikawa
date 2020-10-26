8191f66f0dcd3d897192c617d1cd2b19fb82e443
ChinatsuIshikawa <72743312+ChinatsuIshikawa@users.noreply.github.com>
2020-10-26 11:56:01 +0900

    section2-q4 girisya

--- section2-q4/sketch.js
+++ section2-q4/sketch.js
@@ -9,7 +9,14 @@ function setup() {
 
   for(let i = 0; i < 9; i++){
     // BLANK[1] (hint: 縞の色を交互に変えるには2で割った余りを使おう)
-    rect(0, i * d, width, (i + 1) * d);
+    if((i%2)==0){
+      fill(blue);
+      rect(0, i * d, width, (i + 1) * d);
+    }
+    else{
+      fill(255);
+      rect(0, i * d, width, (i + 1) * d);
+  }
   }
 
   fill(blue);
@@ -17,5 +24,7 @@ function setup() {
   rect(0, 0, size, size);
 
   fill(255);
+  rect(0,40,size,size/4);
+  rect(40,0,size/4,size);
   // BLANK[2] (hint: 白い十字を描くには rect を二つ描こう)
 }
