cec669fa920c70a5ad7377bb2e6c285bd88574db
ChinatsuIshikawa <72743312+ChinatsuIshikawa@users.noreply.github.com>
2020-11-22 15:51:31 +0900

    4-q2

--- section4-q2/sketch.js
+++ section4-q2/sketch.js
@@ -1,4 +1,5 @@
 // テキスト「アニメーション」
+// テキスト「アニメーション」
 let x, y, vx, vy;
 
 function setup(){
@@ -12,7 +13,9 @@ function setup(){
 function draw(){
   background(160, 192, 255);
   // BLANK[2] (hint: 作った star 関数を使います)
-
+  star(x,y,10,vx*vy);
+  x -= 2;
+  y += 2;
   // 端の処理パターン (1) 反対側から出てくる
   if(x > width){ x = 0; }
   else if(x < 0){ x = width; }
@@ -23,10 +26,12 @@ function draw(){
 function star(cx, cy, r, angle){
   beginShape();
   for(var i = 0; i < 20; i++){
-    var theta = TWO_PI * i * 2 / 5 - HALF_PI;
+    var theta = TWO_PI * i * 2 / 5 - HALF_PI + angle;
     // BLANK[1] (hint: angle 分だけ星を回転させるには？)
     var x = cx + cos(theta) * r;
     var y = cy + sin(theta) * r;
+    vx = x * cos(theta) + y * sin(theta) + x;
+    vy = x * sin(theta) - y * cos(theta) + y;
     vertex(x,y);
   }
   endShape(CLOSE);
