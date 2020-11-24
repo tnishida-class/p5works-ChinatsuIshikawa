8d98c9c5bda1debe7e467f3b031fcb0c84fe253f
ChinatsuIshikawa <72743312+ChinatsuIshikawa@users.noreply.github.com>
2020-11-24 09:50:58 +0900

    4-q1 change

--- section4-q1/sketch.js
+++ section4-q1/sketch.js
@@ -1,20 +1,14 @@
 // 練習問題「心臓の鼓動のように大きくなったり小さくなったりする円」
-// 練習問題「心臓の鼓動のように大きくなったり小さくなったりする円」
-let count;
-let cycle;
 
-function setup(){
-  createCanvas(200, 200);
-  count = 0;
-  cycle = 100;
-}
+let count = 1;
+let cycle = 50;
+
 
 function draw(){
   background(160, 192, 255);
   count = (count + 1) % cycle;
-  // BLANK[1]
-  width = 200;
-  hight = 100;
+  width = 100;
+  height = 100;
   size = count;
   ellipse(width / 2, height / 2, size);
 }
