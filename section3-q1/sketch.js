649ffd250b75425d7857c70de9203f5213801209
ChinatsuIshikawa <72743312+ChinatsuIshikawa@users.noreply.github.com>
2020-11-24 09:25:28 +0900

    3-q1 change

--- section3-q1/sketch.js
+++ section3-q1/sketch.js
@@ -3,22 +3,20 @@
 function setup(){
   createCanvas(400, 400);
   background(255);
+  balloon("I love keyakizaka46");
   balloon("I love keyakizaka46",0,0);
 
 
 }
-
 
 function balloon(t,x,y){
   let w = textWidth(t);
   let h = textAscent() + textDescent();
   let p = 50;
   fill(0);　　
-  rect(10, 10, w + p * 2, h + p * 2);
-  triangle(225, 140, 225, 100, 125, 100);
+  rect(0, 0, w + p * 2, h + p * 2);
+  triangle(w + p * 2, h + p * 2 + 20, w + p * 2, h + p * 2, w + p * 2 - 20, h + p * 2);
   fill(255);
-  text(t, p + x, h + p + y);
-
-
-  }
+  text(t, p, h + p);
+}
   
